2da790e4582d3ff127cb76b8d4c16e95630153ee
duyanhtong <143304282+duyanhtong@users.noreply.github.com>
2025-06-13 13:27:00 +0700

    Update constants.dart

--- app_pet/lib/constants.dart
+++ app_pet/lib/constants.dart
@@ -30,9 +30,9 @@ const defaultPadding = 16.0;
 const double exchangeRate = 25195;
 
 const String clientIDPayPal =
-    "AQrbDy3aNylV7l1k4htuiF2dqYgeY9h112kEQJU3KHJuNbfbio9Qy8dnJVsiuMq8tnDbfF4T8hfYMo8N";
+    "";
 const String secretKeyPayPal =
-    "EMiSltda4t_AZ0-rH86fZEyfnzm7zwWNeSwlO2rotkEVTptdR6Lir6uvhmcVdzdyDMu9UF-q7zRFgAql";
+    "";
 
 
 // Form Error
